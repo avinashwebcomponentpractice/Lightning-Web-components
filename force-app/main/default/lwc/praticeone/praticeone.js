e665c3509938dce2477283129e369b6f216c6677
avinashwebcomponentpratice <bayanasaiavinash513@gmail.com>
2019-07-16 04:46:33 +0530

    delete

--- force-app/main/default/lwc/praticeone/praticeone.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, track,api } from 'lwc';
-export default class HelloWorld extends LightningElement {
-    @track greeting = 'World';
-    @api objectApiName;
-    changeHandler(event) {
-        this.greeting = event.target.value;
-        console(this.objectApiName);
-    }
-}
